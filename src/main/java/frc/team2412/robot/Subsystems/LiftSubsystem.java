e2f283f0071a66a79094f7b9e9ecd656ac0fc836
SumedhP <43325361+SumedhP@users.noreply.github.com>
2020-10-26 16:10:45 -0700

    liftsystem  made?

---
+++ src/main/java/frc/team2412/robot/Subsystems/LiftSubsystem.java
@@ -0,0 +1,26 @@
+package frc.team2412.robot.Subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LiftSubsystem extends SubsystemBase {
+
+//	private final CANSparkMax leftMotor;
+//	private final CANSparkMax rightMotor;
+
+	public LiftSubsystem() {
+		
+	}
+
+	public void extend() {
+
+	}
+
+	public void withdraw() {
+
+	}
+	
+	public void set() {
+
+	}
+
+}
