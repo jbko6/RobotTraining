b8e903a9fa26c1b106dc419a52fc9db14c5f5ae7
SumedhP <43325361+SumedhP@users.noreply.github.com>
2020-10-26 16:46:18 -0700

    commit

--- src/main/java/frc/team2412/robot/Subsystems/LiftSubsystem.java
+++ src/main/java/frc/team2412/robot/Subsystems/LiftSubsystem.java
@@ -1,26 +1,38 @@
 package frc.team2412.robot.Subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team2412.robot.Subsystems.constants.LiftConstants;
 
 public class LiftSubsystem extends SubsystemBase {
 
-//	private final CANSparkMax leftMotor;
-//	private final CANSparkMax rightMotor;
+	private final CANSparkMax leftMotor;
+	private final CANSparkMax rightMotor;
+	
+	
 
-	public LiftSubsystem() {
-		
+	public LiftSubsystem(CANSparkMax leftMotor, CANSparkMax rightMotor) {
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		this.rightMotor.setInverted(true);
 	}
 
 	public void extend() {
-
+		setSpeed(LiftConstants.MAX_SPEED);
 	}
 
-	public void withdraw() {
+	public void retract() {
+		setSpeed(-LiftConstants.MAX_SPEED);
+	}
 
+	public void off() {
+		setSpeed(0);
 	}
 	
-	public void set() {
-
+	public void setSpeed(double speed) {
+		leftMotor.set(speed);
+		rightMotor.set(speed);
 	}
 
 }
---
+++ src/main/java/frc/team2412/robot/Subsystems/constants/LiftConstants.java
@@ -0,0 +1,7 @@
+package frc.team2412.robot.Subsystems.constants;
+
+public class LiftConstants {
+	
+	public final static int MAX_SPEED = 1;
+
+}
